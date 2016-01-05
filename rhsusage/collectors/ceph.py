5168a60e43da0158c78f8462db7bbb7966151de7
Paul Cuzner <pcuzner@redhat.com>
2016-01-05 16:20:36 +1300

    1st cut of the ceph collector

--- rhsusage/collectors/ceph.py
+++ rhsusage/collectors/ceph.py
@@ -1,28 +1,60 @@
 __author__ = 'paul'
 
-from rhsusage.tasks import config as cfg
+import json
 
+from rhsusage.tasks import config as cfg
+from rhsusage.tasks.cli import ShellCommand
+from rhsusage.tasks.utils import host_2_ip
 
 class CephCollector(object):
 
     def __init__(self, rrd_db):
-        self.rrd_db = rrd_db
+        #self.rrd_db = rrd_db
         pass
 
     def update(self):
-        cfg.log.info('get ceph stats')
+        #cfg.log.info('get ceph stats')
         cluster_stats = self.get_data()
-        self.rrd_db.update(cluster_stats)
+        print cluster_stats
+        #self.rrd_db.update(cluster_stats)
 
     def get_data(self):
-        # use ceph df, then extract the raw/usable/used
-        # Need to get a total node count for ceph? parse ceph.conf?
-        pass
+        # use ceph df -f json, then extract the raw/usable/used
+        # Need to get a total node count for ceph? parse ceph.conf or use
+        # ceph mon dump -f json and ceph osd dump -f json
+        #
+        ceph_data = {}
+        ceph_nodes = set()
 
-    # def _update_rrd(self):
-    #     cfg.log.debug('updating rrd database')
-    #     # rc = rrdtool.update(self.rrd_db, 'N:%s:%s' %(metric1, metric2));
-    #     pass
+        cmd = ShellCommand('ceph -s -f json')
+        cmd.run()
+        #cfg.log.debug("ceph -s command returned %d" % cmd.rc)
+        if cmd.rc == 0:
 
-    # def _refresh_graph(self):
-    #     cfg.log.debug('refreshing rrd graph')
+            js = json.loads(cmd.stdout[1])
+            ceph_data['used_capacity'] = js['pgmap']['bytes_used']
+            ceph_data['raw_capacity'] = js['pgmap']['bytes_total']
+            ceph_data['usable_capacity'] = 0
+
+            # extract the mon information from the ceph output
+            for mon in js['monmap']['mons']:
+                # node info will look like this - 192.168.122.111:6789/0
+                # and we only want the IP address
+                ceph_nodes.add(mon['addr'].split(':')[0])
+
+            # add the osd nodes to the ceph_nodes list
+            cmd = ShellCommand('ceph osd tree -f json')
+            cmd.run()
+            #cfg.log.debug("ceph osd tree command returned %d" % cmd.rc)
+            if cmd.rc == 0:
+                js = json.loads(cmd.stdout[1])
+                for osd_element in js['nodes']:
+                    if osd_element['type'] == 'host':
+                        hostname = osd_element['name']
+                        if not hostname[0].isdigit():
+                            hostname = host_2_ip(hostname)
+                        ceph_nodes.add(hostname)
+
+            ceph_data['node_count'] = len(ceph_nodes)
+
+        return ceph_data
--- rhsusage/tasks/utils.py
+++ rhsusage/tasks/utils.py
@@ -1,5 +1,11 @@
 __author__ = 'paul'
+import socket
 
 def str2bool(value):
     return value.lower() in ['y','yes','1']
 
+def host_2_ip(hostname):
+    try:
+        return socket.gethostbyname(hostname)
+    except:
+        return hostname
