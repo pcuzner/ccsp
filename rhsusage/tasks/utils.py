26c8b342205827fc410ceec1279e5cb8a5f55b0e
Paul Cuzner <pcuzner@redhat.com>
2016-02-02 16:26:37 +1300

    add function to convert bytes to MB/GB/TB value for display purposes

--- rhsusage/tasks/utils.py
+++ rhsusage/tasks/utils.py
@@ -1,6 +1,7 @@
 __author__ = 'paul'
 import socket
 from dateutil.parser import parse
+import math
 
 
 def str2bool(value):
@@ -12,6 +13,23 @@ def host_2_ip(hostname):
     except:
         return hostname
 
+def readable_bytes(bytes_in, scale, mode='bin'):
+
+    valid_scale = ['KB', 'MB', 'GB', 'TB', 'PB',
+                   'EB', 'ZB', 'YB']
+
+    if scale.upper() not in valid_scale:
+        return 0
+
+    divisor_unit = 1024 if mode == 'bin' else 1000
+    divisor = math.pow(divisor_unit, (valid_scale.index(scale) + 1))
+    rounding = {'K': 0, 'M': 0, 'G': 0, 'T': 1, 'P': 2, 'Z': 2, 'Y': 2}
+    precision = rounding[scale[0]]
+
+    readable = bytes_in / divisor
+
+    return '{0:.{1}f} {2}'.format(readable, precision, scale.upper())
+
 def dates_OK(args, rrd_db):
 
     try:
@@ -19,18 +37,18 @@ def dates_OK(args, rrd_db):
         e_date = parse(args.end_date)
 
     except ValueError:
-        print 'at least one of the date(s) provided is in an invalid format'
+        print 'Error: at least one of the date(s) provided is in an invalid format'
         return False
 
     # quick check that the date range is ok
     if e_date <= s_date:
-        print 'The combination of end date and start date is not valid. Are they reversed?'
+        print 'Error: The combination of end date and start date is not valid. Are they reversed?'
         return False
 
     # now check that the date range is meaningful to the rrd dataset
     if (s_date > rrd_db.rrd_boundary('last') or
         e_date < rrd_db.rrd_boundary('first')):
-        print 'The date range provided is not available within the rrd file'
+        print 'Error: the date range provided is not available within the rrd file'
         return False
 
     # at this point the dates pass the initial 'sniff' test
